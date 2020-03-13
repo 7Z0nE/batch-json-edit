9211daf52663c27925edb62a794ce3255a3f790e
Frederik Wegner <frederik.wegner@merckgroup.com>
2020-03-13 10:09:30 -0700

    support string literal eval for values

--- batch-json-edit.py
+++ batch-json-edit.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python
 import argparse
 import json
 import os
-
+import ast
 
 def parse_folder(root, recursive=False):
     files = [os.path.join(root, name) for name in os.listdir(root) if not os.path.isdir(name)]
@@ -40,56 +41,59 @@ def get_json_nested(json, attr):
 
 def set_json_nested(json, attr, value):
     attrs = attr.split('.')
-    res = json[attrs[0]]
-    for a in attrs[1:-1]:
-        if not a in res:
-            res[a] = {}
-        else:
-            res = res[a]
-    res[attrs[-1]] = value
+    res = json
+    if len(attrs) > 1:
+        for a in attrs[:-1]:
+            if not a in res:
+                res[a] = {}
+            else:
+                res = res[a]
+    res[attrs[-1]] = ast.literal_eval(value)
 
 
 def delete_json_nested(json, attr):
     attrs = attr.split('.')
-    res = json[attrs[0]]
-    for a in attrs[1:-1]:
-        if not a in res:
-            print(res, a)
-            return
-        else:
-            res = res[a]
+    res = json
+    if len(attrs) > 1:
+        for a in attrs[:-1]:
+            if not a in res:
+                print(res, a)
+                return
+            else:
+                res = res[a]
     res.pop(attrs[-1], None)
 
 
-parser = argparse.ArgumentParser('Edit multiple json files of similar structure simultaneously.')
-subparsers = parser.add_subparsers(title='command', dest='command')
+if __name__=="__main__":
+    parser = argparse.ArgumentParser('Edit multiple json files of similar structure simultaneously.')
+    subparsers = parser.add_subparsers(title='command', dest='command')
 
-add_parser = subparsers.add_parser('add', help='Adds an attribute to the specified json files. Errors if the attribute already exists.')
-edit_parser = subparsers.add_parser('edit', help='Edits an attribute. Errors if the attribute does not exist in all files.')
-set_parser = subparsers.add_parser('set', help='Sets an attribute. Ignores whether is already existed or not.')
-delete_parser = subparsers.add_parser('delete', help='Deletes an attribute. Ignores whether it already exists or not.')
+    add_parser = subparsers.add_parser('add', help='Adds an attribute to the specified json files. Errors if the attribute already exists.')
+    edit_parser = subparsers.add_parser('edit', help='Edits an attribute. Errors if the attribute does not exist in all files.')
+    set_parser = subparsers.add_parser('set', help='Sets an attribute. Ignores whether is already existed or not.')
+    delete_parser = subparsers.add_parser('delete', help='Deletes an attribute. Ignores whether it already exists or not.')
+    
+    set_parser.add_argument('field', help='Path to the field to set. E.g. x.y.myfield.')
+    set_parser.add_argument('value', help='value to write to the field')
+    set_parser.add_argument('files', nargs='+', help='files and folders containing json files to edit')
+    set_parser.add_argument('--recursive', nargs='?', default='False', help='whether to recurse into folder of folders')
 
-set_parser.add_argument('field', help='Path to the field to set. E.g. x.y.myfield.')
-set_parser.add_argument('value', help='value to write to the field')
-set_parser.add_argument('files', nargs='+', help='files and folders containing json files to edit')
-set_parser.add_argument('--recursive', nargs='?', default='False', help='whether to recurse into folder of folders')
+    delete_parser.add_argument('field', help='Path to the field to delete. E.g. x.y.myfield.')
+    delete_parser.add_argument('files', nargs='+', help='files and folders containing json files to edit')
+    delete_parser.add_argument('--recursive', nargs='?', type=bool, default='False', help='whether to recurse into folder of folders')
 
-delete_parser.add_argument('field', help='Path to the field to delete. E.g. x.y.myfield.')
-delete_parser.add_argument('files', nargs='+', help='files and folders containing json files to edit')
-delete_parser.add_argument('--recursive', nargs='?', type=bool, default='False', help='whether to recurse into folder of folders')
+    args = parser.parse_args()
 
-args = parser.parse_args()
+    files = parse_files(args.files, recursive=args.recursive)
+    jsons = load_json(files)
+    print(files)
+    for j in jsons:
+        if args.command == 'set':
+            set_json_nested(j, args.field, args.value)
+        elif args.command == 'delete':
+            delete_json_nested(j, args.field)
 
-files = parse_files(args.files, recursive=args.recursive)
-jsons = load_json(files)
-print(files)
-for j in jsons:
-    if args.command == 'set':
-        set_json_nested(j, args.field, args.value)
-    elif args.command == 'delete':
-        delete_json_nested(j, args.field)
-
-for f, j in zip(files, jsons):
-    with open(f, 'w') as ff:
-        json.dump(j, ff, indent=4, sort_keys=False)
+    for f, j in zip(files, jsons):
+        with open(f, 'w') as ff:
+            json.dump(j, ff, indent=4, sort_keys=False)
 
